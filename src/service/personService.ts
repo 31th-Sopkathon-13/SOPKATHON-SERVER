acaa689f1de7f67ffdf5af0489444f500da1653e
Minjae Kang <rkdalswo1021@naver.com>
2022-11-20 04:30:08 +0900

    [CHORE] confilct 해결

--- src/service/personService.ts
+++ src/service/personService.ts
@@ -1,61 +1,52 @@
 import { PersonCreateDto } from './../interface/PersonCreateDto';
-import { PrismaClient } from '@prisma/client'
-import { PersonUpdateDto } from '../interface/PersonUpdateDto';
-const prisma = new PrismaClient()
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
+const createPerson = async (
+  personCreateDto: PersonCreateDto,
+  userId: number,
+) => {
+  const data = await prisma.person.create({
+    data: {
+      user_id: userId,
+      name: personCreateDto.name,
+      birth: personCreateDto.birth,
+      mbti: personCreateDto.mbti,
+      memo: personCreateDto.memo,
+      value: personCreateDto.value,
+    },
+  });
 
+  return data;
+};
 
-const createPerson = async(personCreateDto : PersonCreateDto, userId : number)=>{
-    const data = await prisma.person.create({
-        data:{
-            user_id : userId,
-            name : personCreateDto.name,
-            birth : personCreateDto.birth,
-            mbti : personCreateDto.mbti,
-            memo : personCreateDto.memo,
-            value : personCreateDto.value
+const getPersonById = async (personId: number) => {
+  const data = prisma.person.findUnique({
+    where: {
+      id: personId,
+    },
+  });
 
-        }
-    });
+  return data;
+};
 
+const updatePerson = async (personId: number, isActive: boolean) => {
+  const data = await prisma.person.update({
+    where: {
+      id: personId,
+    },
+    data: {
+      is_active: isActive,
+    },
+  });
 
+  return data;
+};
 
-    return data;
-}
-
-const getPersonById = async(personId : number)=>{
-    const data = prisma.person.findUnique({
-        where : {
-            id : personId
-
-        }
-    });
-
-    return data;
-    
-}
-
-const updatePerson = async( personId : number, isActive : boolean)=>{
-
-
-    const data = await prisma.person.update({
-        where : {
-            id : personId
-        },
-        data : {
-            is_active : isActive,
-        }
-
-    });
-
-    return data;
-
-}
-
-const personService={
-    createPerson,
-    getPersonById,
-    updatePerson
-}
+const personService = {
+  createPerson,
+  getPersonById,
+  updatePerson,
+};
 
 export default personService;
